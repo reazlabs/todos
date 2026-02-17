95c7819ec634c963abbf81f8b313cec7419d7dbc
reazswe <reaz.swe@gmail.com>
2026-02-17 15:23:58 +0600

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
 
-  const [users,setUser] =useState([]);
   return (
     <>
-       <h1>Hello World</h1>
-       <h2>Hello Riaz</h2> 
+       <p>supabase</p> 
     </>
   )
 }
